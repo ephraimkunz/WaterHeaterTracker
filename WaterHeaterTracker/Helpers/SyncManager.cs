85bc15efee9acfe50362334149121911d26497c7
Ephraim Kunz <ephraimkunz@me.com>
2016-12-17 17:47:35 -0700

    Remove the offline sync. Too bugy and not needed.

--- WaterHeaterTracker/Helpers/SyncManager.cs
+++ WaterHeaterTracker/Helpers/SyncManager.cs
@@ -2,17 +2,19 @@
 using Microsoft.WindowsAzure.MobileServices;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+
 namespace WaterHeaterTracker
 {
     public class SyncManager
     {
         public static MobileServiceClient MobileService { get; set; }
-        IMobileServiceTable<WaterHeater> heaterTable;
+        public static IMobileServiceTable<WaterHeater> heaterTable;
 
         public SyncManager()
         {
             MobileService = new MobileServiceClient("https://waterheatertracker.azurewebsites.net");
             heaterTable = MobileService.GetTable<WaterHeater>();
+
         }
 
         public void createHeaterRecord(WaterHeater heater)
