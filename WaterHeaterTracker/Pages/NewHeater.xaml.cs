d8786df9a6a743c48022dc71638fd0492a669449
Ephraim Kunz <ephraimkunz@me.com>
2016-12-24 09:53:39 -0700

    Implement Bradford White year lookup by serial number. Add scroll view.

--- WaterHeaterTracker/Pages/NewHeater.xaml.cs
+++ WaterHeaterTracker/Pages/NewHeater.xaml.cs
@@ -7,9 +7,16 @@ namespace WaterHeaterTracker
 {
     public partial class NewHeater : ContentPage
     {
+        const int DEFAULT_FAILURE_INDEX = 20; //20 years ago default failure time
         public NewHeater()
         {
             InitializeComponent();
+
+            //Disable serial number field by default
+            serialNumber.IsEnabled = false;
+
+            //Don't show the serialYearKey by default
+            serialYearKey.IsVisible = false;
 
             //Add values for manufacturer picker
             var enumVals = EnumUtil.GetManufacturerNames();
@@ -25,7 +32,15 @@ namespace WaterHeaterTracker
             for (int i = DateTime.Now.Year; i >= 1980; i--){
                 manufactured.Items.Add(i.ToString());
             }
-            manufactured.SelectedIndex = 20; //20 years ago default failure time (warranty usually expires here)
+            manufactured.SelectedIndex = DEFAULT_FAILURE_INDEX; 
+        }
+
+        void ManufacturerIndexChanged(object sender, EventArgs e){
+            bool isBradfordWhite = 
+                manufacturer.Items[manufacturer.SelectedIndex] == EnumUtil.ParseManufacturerEnum(Manufacturer.BradfordWhite);
+
+            serialNumber.IsEnabled = isBradfordWhite;
+            serialYearKey.IsVisible = isBradfordWhite;
         }
 
         async void SaveClicked(object sender, EventArgs e){
@@ -43,5 +58,42 @@ namespace WaterHeaterTracker
 
             await Navigation.PopAsync();
         }
+
+        void SerialCompleted(object sender, EventArgs e){
+            var serial = ((Entry)sender).Text;
+            var year = getYearForSerialNumber(serial);
+            manufactured.SelectedIndex = manufactured.Items.IndexOf(year.ToString());
+        }
+
+        int getYearForSerialNumber(string serial){
+            var lookup = new Dictionary<string, int>
+            {
+                {"M", 1995},
+                {"N", 1996},
+                {"P", 1997},
+                {"0", 1997}, //Due to a computer error there were some OA serial water heaters manufactured. They were built in January of 1997.
+                {"S", 1998},
+                {"T", 1999},
+                {"W", 2000},
+                {"X", 2001},
+                {"Y", 2002},
+                {"Z", 2003},
+                {"A", 2004},
+                {"B", 2005},
+                {"C", 2006},
+                {"D", 2007},
+                {"E", 2008},
+                {"F", 2009},
+                {"G", 2010},
+                {"H", 2011},
+                {"J", 2012},
+                {"K", 2013},
+                {"L", 2014}
+            };
+            if(serial.Length > 0 && lookup.ContainsKey(serial[0].ToString().ToUpper()))
+                return lookup[serial[0].ToString().ToUpper()];
+            return Int32.Parse(manufactured.Items[DEFAULT_FAILURE_INDEX]);
+        }
+
     }
 }
